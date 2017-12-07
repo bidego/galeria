1e6e28ab9ca228dc5a462d1f07c8a236ce35bcd6
Gonzalo <gonzalo.bide@gmail.com>
2017-12-06 22:09:11 -0300

    code splitted and cleaned up

--- public/main.js
+++ public/main.js
@@ -1,8 +1,12 @@
 angular.module('angularGaleria', [])
 
 function mainController($scope, $http) {
-    $scope.formData = {}
 
+    // INITIALIZE GLOBAL VARS FOR BINDING DATA
+    $scope.formData = {}
+    $scope.galeriaData = {}
+
+    // LOAD SECTIONS ON PAGE LOADED
     //Cuando se cargue la pagina, pide las galerias de mongodb a la api
     $http.get('/api/galerias')
         .success( data => {
@@ -13,6 +17,7 @@ function mainController($scope, $http) {
             console.log('Error: ' + data)
         })
     
+    // CREATE SECCTION
     //Enviamos el texto a la api al añadir nueva galeria.
     $scope.createGaleria = () => {
         $http.post('/api/galerias', $scope.formData)
@@ -26,15 +31,43 @@ function mainController($scope, $http) {
             })
     }
 
-    //Borramos una galeria a través de la api
-    $scope.deleteGaleria = id => {
-        $http.delete('/api/galerias/' + id)
+    // READ SECTION DETAILS
+    $scope.showDetails = id => {
+        $http.get('/api/galeria/'+id)
             .success( data => {
-                $scope.galerias = data
-                console.log(data)
-            })
-            .error( data => {
-                console.log('Error : ' + data)
+                $scope.details = data;
             })
     }
+
+    // ADD PHOTO TO SECTIONS
+    $scope.addPhotoToGaleria = (id) => {
+        $http({ url: '/api/galeria/uploadPhoto/'+id, method:'POST', data: $scope.galeriaData, headers: {'Content_type': 'multipart/form-data'} })
+            .success( data => {
+                $scope.formAddPhoto = data
+                console.log(data)
+            })
+    }
+
+    // DELETE SECTION
+    //Borramos una galeria a través de la api
+    $scope.deleteGaleria = id => {
+        if(confirm("Do you want to delete this collection?")) {
+            $http.delete('/api/galerias/' + id)
+                .success( data => {
+                    $scope.galerias = data
+                    console.log(data)
+                })
+                .error( data => {
+                    console.log('Error : ' + data)
+                })
+        }
+    }
+        /*
+    var socket = io()
+    socket.on('connect', () => {
+        socket.emit('ferret', 'tobi', data => {
+            console.log(data)
+        })
+    })*/
+
 }
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express')
 var mongoose = require('mongoose')
+var jobs = require('./jobs.js')
+
+var fs = require('fs')
+var formidable = require('formidable')
+var path = require('path')
 
 mongoose.Promise = global.Promise;mongoose.connect('mongodb://localhost:27017/galeria')
 
@@ -13,19 +18,17 @@ var bodyParser = require('body-parser')
 
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended:true}))
+app.use(bodyParser.urlencoded({ extended:false}))
 app.use(express.static(__dirname + '/public'))
 
 //app.use(multer({dest: './uploads/'}))
 
 var nameSchema = new mongoose.Schema({
     name: String,
-    description: String
-/*    photos: [{
-        url: String,
-        title: String,
-        description: String
-    }]*/
+    description: String,
+    photos: [{
+        url: String
+    }]
 });
 
 //Definimos modelos.
@@ -41,6 +44,13 @@ app.get('/api/galerias', (req, res) => {
 		res.json(galerias);
 	});
 });
+
+app.get('/api/galeria/:id', (req, res) => {
+	Galeria.find({_id:req.params.id}, (err, galeria) => {
+		if(err) { res.send(err) }
+		res.json(galeria)
+	})
+})
 
 // POST que crea una GALERIA y devuelve todos tras la creación
 app.post('/api/galerias', (req, res) => {
@@ -62,10 +72,34 @@ app.post('/api/galerias', (req, res) => {
 	});
 });
 
-app.post('/upload', (req,res) => {
-    console.log(req.body)
-    console.log(req.files)
-    res.json({success: true})
+app.post('/api/galeria/uploadPhoto/:id', (req,res) => {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var old_path = files.filenew.path,
+            file_size = files.filenew.size,
+            file_ext = files.filenew.name.split('.').pop(),
+            index = old_path.lastIndexOf('/') + 1,
+            file_name = old_path.substr(index),
+            new_path = path.join(process.env.PWD, '/uploads/', file_name + '.' + file_ext);
+/*
+        fs.readFile(old_path, function(err, data) {
+            fs.writeFile(new_path, data, function(err) {
+                fs.unlink(old_path, function(err) {
+                    if (err) {
+                        res.status(500);
+                        res.json({'success': false});
+                    } else {
+                        res.status(200);
+                        res.json({'success': true});
+                    }
+                });
+            });
+        });*/
+    });
+
+    Galeria.update( { "_id": req.params.id }, {"$push": { photos: req.body.filename }}, {"upsert":true})
+
+    res.json({success: true, changed: 12 })
 })
 
 // DELETE un TODO específico y devuelve todos tras borrarlo.
@@ -90,12 +124,16 @@ app.delete('/api/galerias/:galeria', (req, res) => {
 // Carga una vista HTML simple donde irá nuestra Single App Page
 // Angular Manejará el Frontend
 
-app.get('/addPhoto', (req, res) => {						
+app.post('/addPhoto', (req, res) => {						
 	res.sendFile(__dirname+'/public/addPhoto.html');
 });
 
-app.get('*', (req, res) => {						
-	res.sendFile(__dirname+'/public/galeria.html');				
+app.get('/', (req, res) => {						
+	res.sendFile(__dirname+'/public/galeria.html');
+});
+
+app.get('/public/main.js', (req, res) => {                        
+    res.sendFile(__dirname+'/public/main.js');
 });
 
 io.on('connection', socket => {
